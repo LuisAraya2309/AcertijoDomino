d73e80727bf5dd9fd8e614b61cbe699a71585e91
Luis <luistec2309@estudiantec.cr>
2021-09-06 13:39:11 -0600

    Inicio de pagina web

--- src/index.py
+++ src/index.py
@@ -1,5 +1,5 @@
 #Libraries
-from flask import Flask
+from flask import Flask,render_template,request
 
 app = Flask(__name__)
 
@@ -7,7 +7,16 @@ app = Flask(__name__)
 #Routes
 @app.route('/',methods=['GET'])
 def principal():
-    return "Bienvenido a la pagina web"
+    return render_template('index.html')
+
+@app.route('/eleccion',methods=['POST','GET'])
+def solucion():
+    algoritmo = request.form['algoritmo']
+    set = request.form['set']
+    if algoritmo == 'backtracking':
+        return render_template('backtracking.html',set = set)
+
+    return render_template('fuerzaBruta.html',set = set)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=4000,debug=True)
